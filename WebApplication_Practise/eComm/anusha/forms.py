f352de8dd25d2c81da8670dc1ef2b6af95cb7048
apanidar <apanidar@cisco.com>
2020-10-24 21:56:55 +0530

    adding webapplication project

---
+++ WebApplication_Practise/eComm/anusha/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+'''from .models import UserRegistration
+class Registration(forms.ModelForm):
+    class Meta:
+        model = UserRegistration
+        fields = '__all__'
+
+'''
